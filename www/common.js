c7733fc5be5bda2266060cf34a86ebd1964e83b2
xuqiang <352789535@qq.com>
2016-12-13 17:48:43 +0800

    数据反向流动

--- www/common.js
+++ www/common.js
@@ -9,15 +9,36 @@ var data=[
 ];
 //搜索条
 var SearchBar=React.createClass({
+	handleChange:function(){
+		this.props.onUserInput(
+			this.refs.filterTextInput.value,
+			this.refs.inStockOnlyInput.checked
+		)
+	},
 	render:function(){
 		return (
 			<form>
-				<input type="text" placeholder="Search.."/>
-				<label><input type="checkbox"/>股票相关</label>
+				<input type="text"
+				       placeholder="Search.."
+				       value={this.props.filterText}
+				       ref="filterTextInput"
+								onChange={this.handleChange}
+				/>
+				<p>
+					<input
+						type="checkbox"
+						checked={this.props.inStockOnly}
+					  ref="inStockOnlyInput"
+					  onChange={this.handleChange}
+					/>
+					{' '}
+					Only show products in stock
+				</p>
 			</form>
 		)
 	}
 });
+
 //数据表头
 var ProductCategoryRow=React.createClass({
 	render:function(){
@@ -34,7 +55,7 @@ var ProductRow=React.createClass({
 			this.props.product.name:
 			<span style={{color:'red'}}>
 						{this.props.product.name}
-					</span>;
+			</span>;
 		return(
 			<tr>
 				<td>{name}</td>
@@ -43,18 +64,22 @@ var ProductRow=React.createClass({
 		)
 	}
 });
+
 //数据集合
 var ProductTable=React.createClass({
 	render:function(){
 		var rows=[];
 		var lastCategory=null;
 		this.props.product.forEach(function(pro){
+			if(pro.name.indexOf(this.props.filterText)===-1 || (!pro.stocked&&this.props.inStockOnly)){
+				return ;
+			}
 			if(lastCategory!=pro.category){
 				rows.push(<ProductCategoryRow product={pro} key={pro.category}/>);
-				lastCategory=pro.category;
 			}
 			rows.push(<ProductRow product={pro} key={pro.name}/>);
-		})
+			lastCategory=pro.category;
+		}.bind(this));
 		return (
 			<table>
 				<thead>
@@ -70,13 +95,32 @@ var ProductTable=React.createClass({
 		)
 	}
 });
+
 //表单框
 var FilterableProductTable=React.createClass({
+	getInitialState:function(){
+		return{
+			filterText: '',
+			inStockOnly: false
+		}
+	},
+	handleUserInput:function(filterText,inStockOnly){
+		this.setState({
+			filterText:filterText,
+			inStockOnly:inStockOnly
+		})
+	},
 	render:function(){
 		return (
 			<div>
-				<SearchBar/>
-				<ProductTable product={this.props.product}/>
+				<SearchBar
+					onUserInput={this.handleUserInput}
+					filterText={this.state.filterText}
+					inStockOnly={this.state.inStockOnly}/>
+				<ProductTable
+					product={this.props.product}
+					filterText={this.state.filterText}
+					inStockOnly={this.state.inStockOnly}/>
 			</div>
 		)
 	}
@@ -85,4 +129,4 @@ var FilterableProductTable=React.createClass({
 ReactDOM.render(
 	<FilterableProductTable product={data}/>,
 	document.getElementById('content')
-)
+);
